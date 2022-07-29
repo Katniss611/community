09bb42399509add023012c47147696a80ad7ed36
cpcat_611 <674430327@qq.com>
2022-07-29 23:41:01 +0800

    实现关注、取消关注以及查看用户关注和粉丝功能。

--- src/main/java/com/tangerine/community/util/RedisKeyUtil.java
+++ src/main/java/com/tangerine/community/util/RedisKeyUtil.java
@@ -7,6 +7,9 @@ public class RedisKeyUtil {
 
     private static final String PREFIX_USER_LIKE = "like:user";
 
+    private static final String PREFIX_FOLLOWEE = "followee";
+    private static final String PREFIX_FOLLOWER = "follower";
+
 
     // 某个实体的赞的format
     // like:entity:entityType:entityId -> Set(userId)
@@ -19,5 +22,18 @@ public class RedisKeyUtil {
     public static String getUserLikeKey(int userId) {
         return PREFIX_USER_LIKE + SPLIT + userId;
     }
+
+    // 某个用户关注的实体
+    // followee:userId:entityType -> zset(entityId,now)
+    public static String getFolloweeKey(int userId, int entityType) {
+        return PREFIX_FOLLOWEE + SPLIT + userId + SPLIT + entityType;
+    }
+
+    // 某个实体拥有的粉丝
+    // follower:entityType:entityId -> zset(userId,now)
+    public static String getFollowerKey(int entityType, int entityId) {
+        return PREFIX_FOLLOWER + SPLIT + entityType + SPLIT + entityId;
+    }
+
 
 }
