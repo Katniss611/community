a2ecc43760fa774c73028639228050ffa2f104f6
cpcat_611 <674430327@qq.com>
2022-07-30 23:30:07 +0800

    登录模块优化。用Redis缓存验证码、用户登录凭证和用户信息。

--- src/main/java/com/tangerine/community/controller/UserController.java
+++ src/main/java/com/tangerine/community/controller/UserController.java
@@ -2,6 +2,8 @@ package com.tangerine.community.controller;
 
 import com.tangerine.community.annotation.LoginRequired;
 import com.tangerine.community.entity.User;
+import com.tangerine.community.service.FollowService;
+import com.tangerine.community.service.LikeService;
 import com.tangerine.community.service.UserService;
 import com.tangerine.community.util.CommunityUtil;
 import com.tangerine.community.util.HostHolder;
@@ -24,6 +26,8 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Map;
 
+import static com.tangerine.community.util.CommunityConstant.ENTITY_TYPE_USER;
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
@@ -43,6 +47,12 @@ public class UserController {
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private LikeService likeService;
+
+    @Autowired
+    private FollowService followService;
 
     @LoginRequired
     @RequestMapping(path = "/setting", method = RequestMethod.GET)
@@ -120,5 +130,36 @@ public class UserController {
         }
     }
 
+    // 个人主页
+    @RequestMapping(path = "/profile/{userId}", method = RequestMethod.GET)
+    public String getProfilePage(@PathVariable("userId") int userId, Model model) {
+        User user = userService.findUserById(userId);
+        if (user == null) {
+            throw new RuntimeException("该用户不存在!");
+        }
+
+        // 用户
+        model.addAttribute("user", user);
+        // 点赞数量
+        int likeCount = likeService.findUserLikeCount(userId);
+        model.addAttribute("likeCount", likeCount);
+
+        // 关注数量
+        long followeeCount = followService.findFolloweeCount(userId, ENTITY_TYPE_USER);
+        model.addAttribute("followeeCount", followeeCount);
+        // 粉丝数量
+        long followerCount = followService.findFollowerCount(ENTITY_TYPE_USER, userId);
+        model.addAttribute("followerCount", followerCount);
+        // 是否已关注
+        boolean hasFollowed = false;
+        if (hostHolder.getUser() != null) {
+            hasFollowed = followService.hasFollowed(hostHolder.getUser().getId(), ENTITY_TYPE_USER, userId);
+        }
+        model.addAttribute("hasFollowed", hasFollowed);
+
+        return "/site/profile";
+    }
+
 
 }
+
